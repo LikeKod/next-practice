d5b9c1b137214c6c950153e31be21abb21a48d97
LikeKod <i.serebryanoy@yandex.ru>
2024-03-12 23:14:14 +0300

    don't_work-eslynt!

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Head } from "next/document";
+import { Head, NextScript } from "next/document";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head />
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}><main>{children}</main><NextScript /></body>
     </html>
   );
 }
