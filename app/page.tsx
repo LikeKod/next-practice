e15e0188055b7c7db222ec23e9885bb7c5c87569
LikeKod <i.serebryanoy@yandex.ru>
2024-03-22 23:34:48 +0300

    start_layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,22 @@
+'use client';
 
-import { Metadata } from "next";
+import { useState } from "react";
+import { Button, Htag, Paragraph, Rating } from "../components/index";
+import Logo from '../public/vercel.svg';
+
+// import { Metadata } from "next";
 // import { AppProps } from "next/dist/shared/lib/router/router";
 import Head from "next/head";
 
-export const metadata: Metadata = {
-  title: "My training project page",
-  description: "Start develop",
-  authors: [{ name: "Likie" }],
-};
+// export const metadata: Metadata = {
+//   title: "My training project page",
+//   description: "Start develop",
+//   authors: [{ name: "Likie" }],
+// };
 
 export default function Home(): JSX.Element {
+  const [rating, setRating] = useState<number>(4);
+
   return (
     <>
       <Head>
@@ -18,6 +25,13 @@ export default function Home(): JSX.Element {
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100..900&display=swap" rel="stylesheet" />
       </Head>
+      <Htag tag='h1'>Text</Htag>
+      <Logo />
+      <Button appearance="primary" arrow="down">Button</Button>
+      <Paragraph size="l">Big</Paragraph>
+      <Paragraph>Medium</Paragraph>
+      <Paragraph size="s">Small</Paragraph>
+      <Rating rating={rating} isEditable setRating={setRating} />
       {/* <Component {...pageProps} /> */}
     </>
   );
