dd48aea92662a49b85f480b74aad9a17c5f7749c
LikeKod <i.serebryanoy@yandex.ru>
2024-04-28 22:46:43 +0300

    add_search

--- components/SideBar/SideBar.tsx
+++ components/SideBar/SideBar.tsx
@@ -3,12 +3,14 @@ import {SideBarProps} from './SideBar.props';
 import Logo from '../logo.svg';
 import styles from './SideBar.module.css';
 import cn from 'clsx';
+import { Search } from '../Search/Search';
 
 
 export const SideBar = ({className, ...props}: SideBarProps): JSX.Element => {
     return (
         <div className={cn(className, styles.sidebar)} {...props}>
             <Logo className={styles.logo}/>
+            <Search />
             <Menu />
         </div>
     );
