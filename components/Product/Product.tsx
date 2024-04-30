cc3b0dc5dfd5402614613a2de2c133bd4a3c31fc
LikeKod <i.serebryanoy@yandex.ru>
2024-04-30 17:26:51 +0300

    product_grid

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -3,12 +3,13 @@ import { ProductProps } from "./Product.props";
 import styles from './Product.module.css';
 import { Rating } from "../Rating/Rating";
 import { Tag } from "../Tag/Tag";
+import { Button } from "../Button/Button";
 
-export const Product = ({product, className, ...props}: ProductProps): JSX.Element => {
+export const Product = ({ product, className, ...props }: ProductProps): JSX.Element => {
     return (
-        <Card>
+        <Card className={styles.product}>
             <div className={styles.logo}>
-                <img src={product.image} alt={product.title} />
+                <img src={process.env.NEXT_PUBLIC_DOMAIN+product.image} alt={product.title} />
             </div>
             <div className={styles.title}>
                 {product.title}
@@ -24,6 +25,37 @@ export const Product = ({product, className, ...props}: ProductProps): JSX.Eleme
             </div>
             <div className={styles.tags}>
                 {product.categories.map(c => <Tag key={c} color="ghost">{c}</Tag>)}
+            </div>
+            <div className={styles.priceTitle}>
+                Price
+            </div>
+            <div className={styles.creditTitle}>
+                Credit
+            </div>
+            <div className={styles.rateTitle}>
+                {product.reviewCount} reviews
+            </div>
+            <div className={styles.hr}><hr /></div>
+            <div className={styles.description}>
+                {product.description}
+            </div>
+            <div className={styles.feature}>
+                feature
+            </div>
+            <div className={styles.advBlock}>
+                <div className={styles.advantages}>
+                   <div>Advantages</div>
+                   <div>{product.advantages}</div>
+                </div>
+                <div className={styles.disadvantages}>
+                   <div>Disdvantages</div>
+                   <div>{product.disadvantages}</div>
+                </div>
+            </div>
+            <div className={styles.hr}><hr /></div>
+            <div className={styles.actions}>
+                <Button appearance="primary">About</Button>
+                <Button appearance="ghost" arrow={'right'}>Read reviews</Button>
             </div>
         </Card>
     );
