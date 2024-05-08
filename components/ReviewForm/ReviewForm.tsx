eafda29aa46a50ac59b6087f257a16c7fe87a466
LikeKod <i.serebryanoy@yandex.ru>
2024-05-08 17:15:22 +0300

    adapt_form

--- components/ReviewForm/ReviewForm.tsx
+++ components/ReviewForm/ReviewForm.tsx
@@ -1,5 +1,6 @@
 import { ReviewFormProps } from "./ReviewForm.props";
 import styles from './ReviewForm.module.css';
+import CloseIcon from './close.svg';
 import cn from 'clsx';
 import { Input } from "../Input/Input";
 import { Rating } from "../Rating/Rating";
@@ -8,18 +9,28 @@ import { TextArea } from "../TextArea/TextArea";
 export const ReviewForm = ({ prductId, children, className, ...props }: ReviewFormProps): JSX.Element => {
 
     return (
-        <div className={cn(styles.reviewForm, className)} {...props}>
-            <Input />
-            <Input />
-            <div className={styles.rating}>
-                <span>Rating:</span>
-                <Rating rating={0}/>
+        <>
+            <div className={cn(styles.reviewForm, className)} {...props}>
+                <Input placeholder="Name" />
+                <Input placeholder="Title review" className={styles.title} />
+                <div className={styles.rating}>
+                    <span>Rating:</span>
+                    <Rating rating={0} />
+                </div>
+                <TextArea placeholder="Review contain" className={styles.description} />
+                <div className={styles.submit}>
+                    <Button appearance='primary'>Send</Button>
+                    <span className={styles.info}>*Before public text will be examination</span>
+                </div>
             </div>
-            <TextArea className={styles.description}/>
-            <div className={styles.submit}>
-                <Button appearance='primary'>Send</Button>
-                <span>*Before public text will be examination</span>
+            <div className={styles.success}>
+                <div className={styles.successTitle}>Your review be send</div>
+                <div>
+                    Thanks for your review!
+                </div>
+                <CloseIcon className={styles.close}/>
             </div>
-        </div>
+
+        </>
     );
 };
