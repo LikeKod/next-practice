7de504fe2f8ebf876dcb075e0b3ba5a400626423
LikeKod <i.serebryanoy@yandex.ru>
2024-06-02 19:35:53 +0300

    start_aria-attributes

--- components/Sort/Sort.tsx
+++ components/Sort/Sort.tsx
@@ -1,27 +1,34 @@
-import {SortEnum, SortProps} from './Sort.props';
+import { SortEnum, SortProps } from './Sort.props';
 import SortIcon from './Sort.svg';
 import cn from 'clsx';
 import styles from './Sort.module.css';
 
 
-export const Sort = ({sort, setSort, className, ...props}: SortProps): JSX.Element => {
+export const Sort = ({ sort, setSort, className, ...props }: SortProps): JSX.Element => {
     return (
         <div className={cn(styles.sort, className)} {...props}>
-            <button 
+            <div className={styles.sortName} id='sort'>Sort</div>
+            <button
+                id='rating'
                 onClick={() => setSort(SortEnum.Rating)}
                 className={cn({
                     [styles.active]: sort == SortEnum.Rating
                 })}
+                aria-selected={sort == SortEnum.Rating}
+                aria-labelledby='sort rating'
             >
-                <SortIcon className={styles.sortIcon}/> Rating
+                <SortIcon className={styles.sortIcon} /> Rating
             </button>
-            <button 
+            <button
+                id='price'
                 onClick={() => setSort(SortEnum.Price)}
                 className={cn({
                     [styles.active]: sort == SortEnum.Price
                 })}
+                aria-selected={sort == SortEnum.Price}
+                aria-labelledby='sort price'
             >
-                <SortIcon className={styles.sortIcon}/> Price
+                <SortIcon className={styles.sortIcon} /> Price
             </button>
         </div>
     );
